6347471083039fd9cf8deaade44058140a50b022
ali khan <alikhan40310@gmail.com>
2022-03-21 13:15:02 +0500

    full functionality completed of Vanilla JavaScript

---
+++ README.md
@@ -0,0 +1,28 @@
+# Canvas App using Vanilla JavaScript
+
+Using canvas create a drawing application with Awesome UI/Ux experience
+
+## Main Components Required
+
+Using different components are follows.
+
+### Font Awesome
+
+Using Font Awesome Icons in dashboard of canvas. These icons are e.g download, erase, save, undo, redo etc.
+
+### BootStrap
+
+Using BootStrap for the purpose of responsiveness of the canvas
+
+### SASS
+
+Style the canvas by using SASS (Syntatically Awesome Style Sheet).
+
+## Main Highlights
+
+In this project user can drawing a painting with different color, also change the thickness of the pen.
+
+### Editable Canvas
+
+User can also undo, redo & erase the canvas if they think that the some mistake happen on the canvas board.
+
--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,17 @@
 function colorChange(data) {
-    color =  data;
-    document.getElementById('editBox').style.border = `2px solid ${color}`;
+    color = data;
+    document.getElementById("editBox").style.border = `2px solid ${color}`;
 }
+
 window.addEventListener("load", () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    // resizing
+
+    // Set canvas size
     canvas.width = 650;
     canvas.height = 350;
-    // event listeners
+
+    //event listeners
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("touchstart", startPosition);
     canvas.addEventListener("mouseup", endPosition);
@@ -18,10 +21,80 @@ window.addEventListener("load", () => {
     document.querySelector("#clear").addEventListener("click", clearCanvas);
     document.querySelector("#undo").addEventListener("click", undo);
     document.querySelector("#save").addEventListener("click", save);
-    // variables
+
+    //variables
     let painting = false;
     let restore = [];
+    let i = -1;
 
+    //functions
+    function startPosition(e) {
+        painting = true;
+        draw(e);
+    }
+    // creating a end position function
+    function endPosition() {
+        painting = false;
+        ctx.beginPath();
+        if (!painting) {
+            restore.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+            i += 1;
+        }
+    }
+    // draw function
+    function draw(e) {
+        if (!painting) {
+            return;
+        }
 
+        e.preventDefault();
+        ctx.lineWidth = document.getElementById("vol").value;
+        ctx.lineCap = "round";
 
+        //adds a new point and creates a lineTo
+        if (e.type == "touchmove") {
+            ctx.lineTo(
+                e.touches[0].clientX - canvas.offsetLeft,
+                e.touches[0].clientY - canvas.offsetTop
+            );
+        } else if (e.type == "mousemove") {
+            ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        }
+        //draw graphics as per context object
+        ctx.stroke();
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        //moves the path to the specified point
+        if (e.type == "touchmove") {
+            ctx.moveTo(
+                e.touches[0].clientX - canvas.offsetLeft,
+                e.touches[0].clientY - canvas.offsetTop
+            );
+        } else if (e.type == "mousemove") {
+            ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        }
+    }
+    //clear all canvas
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        restore.length = 0;
+        i = -1;
+    }
+    //undo drawing on canvas
+    function undo() {
+        if (i <= 0) {
+            clearCanvas();
+        } else {
+            i -= 1;
+            restore.pop();
+            ctx.putImageData(restore[i], 0, 0);
+        }
+    }
+    //Save drawings in system
+    function save() {
+        let link = document.createElement("a");
+        link.download = "download.png";
+        link.href = canvas.toDataURL();
+        link.click();
+    }
 });
